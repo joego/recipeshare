a224bec2db9b45775371c98a21ecbbae23a43ded
jose <jose@yaqom.de>
2015-08-27 16:54:23 +0200

    Email feature updated.

--- client/views/email.js
+++ client/views/email.js
@@ -5,6 +5,8 @@ Template.mail.events({
         mailText = $('.mail-message').val();
     
     Meteor.call('sendEmail', mailWriter, mailText, function(){
+      // Clear fields
+      $('.mail-field').val("");
       // Insert message on page
       $('<div class="alert alert-info">Your message has been sent.</div>').insertAfter('h2');
       // Remove message after 5 seconds
