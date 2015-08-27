eaf988aef5a8c88227fbf503e15179cc66c2598b
jose <jose@yaqom.de>
2015-08-27 16:49:38 +0200

    Email feature updated.

--- client/views/email.js
+++ client/views/email.js
@@ -3,8 +3,11 @@ Template.mail.events({
     e.preventDefault();
     var mailWriter = $('.author-name').val() + ' <' + $('.author-email').val() + '>',
         mailText = $('.mail-message').val();
+    
     Meteor.call('sendEmail', mailWriter, mailText, function(){
-      $('.mail-form').prepend('<div class="alert-info">Your message has been sent.</div>');
+      // Insert message on page
+      $('<div class="alert alert-info">Your message has been sent.</div>').insertAfter('h2');
+      // Remove message after 5 seconds
       setTimeout(function(){
         $('.alert-info').remove();
       }, 5000);
